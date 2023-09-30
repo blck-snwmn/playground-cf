2ac47582146c230e175ca31f1bb4c8992e44632b
blck-snwmn <whitesnowmancreator@gmail.com>
2023-09-30 14:49:55 +0900

    Add AI sample

--- sample-ai/src/worker.ts
+++ sample-ai/src/worker.ts
@@ -1,32 +1,23 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { Ai } from '@cloudflare/ai'
 
 export interface Env {
-	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	// MY_KV_NAMESPACE: KVNamespace;
-	//
-	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-	// MY_DURABLE_OBJECT: DurableObjectNamespace;
-	//
-	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-	// MY_BUCKET: R2Bucket;
-	//
-	// Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
-	// MY_SERVICE: Fetcher;
-	//
-	// Example binding to a Queue. Learn more at https://developers.cloudflare.com/queues/javascript-apis/
-	// MY_QUEUE: Queue;
+	AI: any;
 }
 
 export default {
-	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World!');
+	async fetch(request: Request, env: Env) {
+		const url = new URL(request.url);
+		const sys = url.searchParams.get('sys') ?? "math teacher"
+		const user = url.searchParams.get('user') ?? "write your favorite formula"
+
+		const ai = new Ai(env.AI);
+
+		const messages = [
+			{ role: 'system', content: sys },
+			{ role: 'user', content: user }
+		];
+		const response = await ai.run('@cf/meta/llama-2-7b-chat-int8', { messages });
+
+		return Response.json(response);
 	},
 };
