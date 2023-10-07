6df82fc6a1c5ff010301850367705e37011bd915
blck-snwmn <whitesnowmancreator@gmail.com>
2023-10-08 03:33:26 +0900

    Return message using data in vector db

--- langchain-worker-ai/src/worker.ts
+++ langchain-worker-ai/src/worker.ts
@@ -6,8 +6,10 @@ import type {
 	Request,
 } from "@cloudflare/workers-types";
 
+import { Ai } from '@cloudflare/ai'
 import { CloudflareVectorizeStore } from "langchain/vectorstores/cloudflare_vectorize";
 import { CloudflareWorkersAIEmbeddings } from "langchain/embeddings/cloudflare_workersai";
+import { CheerioWebBaseLoader } from "langchain/document_loaders/web/cheerio";
 
 export interface Env {
 	VECTORIZE_INDEX: VectorizeIndex;
@@ -17,6 +19,11 @@ export interface Env {
 export default {
 	async fetch(request: Request, env: Env) {
 		const { pathname } = new URL(request.url);
+		if (pathname.startsWith("/favicon")) {
+			// 404 for favicon
+			return new Response('', { status: 404 });
+		}
+
 		const embeddings = new CloudflareWorkersAIEmbeddings({
 			binding: env.AI,
 			modelName: "@cf/baai/bge-small-en-v1.5",
@@ -25,41 +32,51 @@ export default {
 			index: env.VECTORIZE_INDEX,
 		});
 
-		if (pathname === "/") {
-			const results = await store.similaritySearch("hello", 1);
-			return Response.json(results);
+		if (pathname === "/search") {
+			const ai = new Ai(env.AI);
+
+			const query = "What is Stream"
+			const results = await store.similaritySearch(query, 5);
+			console.log("results:", results);
+
+			const pcs = results.map((result) => result.pageContent).join("\n");
+
+			const messages = [
+				{ role: 'user', content: query },
+				{ role: 'system', content: "supplement\n" + pcs },
+			];
+			const response = await ai.run('@cf/meta/llama-2-7b-chat-int8', { messages });
+			return Response.json(response);
 		}
 
 		if (pathname === "/load") {
-			// Upsertion by id is supported
-			await store.addDocuments(
-				[
-					{
-						pageContent: "hello",
-						metadata: {
-							url: "https://example.com/hello",
-						},
-					},
-					{
-						pageContent: "world",
-						metadata: {
-							url: "https://example.com/world",
-						},
-					},
-					{
-						pageContent: "hi",
-						metadata: {
-							url: "https://example.com/hi",
-						},
-					},
-				],
-				{ ids: ["id1", "id2", "id3"] }
+			console.log("Loading documents...");
+			const loader = new CheerioWebBaseLoader(
+				"https://datatracker.ietf.org/doc/html/rfc9000",
+				{
+					selector: "#content"
+				},
 			);
-			return Response.json({ success: true });
-		}
+			const docs = await loader.loadAndSplit();
+			// docs.forEach((doc) => {
+			// 	// show meetadata type
+			// 	console.log(doc.metadata);
+			// 	console.log(Object.keys(doc.metadata));
+			// 	console.log("doc.metadata.loc:", doc.metadata.loc);
+			// 	console.log("doc.metadata.loc keys:", Object.keys(doc.metadata.loc));
+			// 	console.log("doc.metadata.loc.lines:", doc.metadata.loc.lines);
+			// 	console.log("doc.metadata.loc.lines keys:", Object.keys(doc.metadata.loc.lines));
 
-		if (pathname === "/clear") {
-			await store.delete({ ids: ["id1", "id2", "id3"] });
+			// 	console.log(doc.pageContent);
+			// 	console.log("===================")
+			// });
+			const nd = docs.map((doc) => {
+				// remove metadata because metadata type is mismatched
+				doc.metadata = null;
+				return doc;
+			})
+			console.log("Loaded documents:", docs.length);
+			const ids = await store.addDocuments(nd);
 			return Response.json({ success: true });
 		}
 
