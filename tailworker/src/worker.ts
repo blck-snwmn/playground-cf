cb014275a7c0ff8137fa24980574cc68bd6821d4
blck-snwmn <whitesnowmancreator@gmail.com>
2023-11-09 23:57:00 +0900

    Send slack

--- tailworker/src/worker.ts
+++ tailworker/src/worker.ts
@@ -1,9 +1,18 @@
 export interface Env {
 	TAIL_SAMPLE: KVNamespace
+	SQUEUE: Queue;
 }
 
 export default {
 	async tail(events: TraceItem[], env: Env, ctx: ExecutionContext) {
 		await env.TAIL_SAMPLE.put('trace', JSON.stringify(events));
+
+		await env.SQUEUE.send({
+			type: "chat.postMessage",
+			body: {
+				channel: "channel_id",
+				text: "hello",
+			},
+		});
 	},
 };
