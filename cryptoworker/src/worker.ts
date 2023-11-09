286fe7f97f61432b1b41f305ec721889470bcc09
blck-snwmn <whitesnowmancreator@gmail.com>
2023-11-10 00:05:21 +0900

    Add crypto sample

---
+++ cryptoworker/src/worker.ts
@@ -0,0 +1,8 @@
+export interface Env {
+}
+
+export default {
+	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+		return new Response(crypto.randomUUID());
+	},
+};
