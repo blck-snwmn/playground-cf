6ad774b4d5c653265a1a857dca7c60df2f588416
blck-snwmn <whitesnowmancreator@gmail.com>
2023-11-22 02:27:37 +0900

    Add new project

---
+++ workertest/src/worker.ts
@@ -0,0 +1,12 @@
+// biome-ignore lint/complexity/noBannedTypes: sample
+export type Env = {};
+
+export default {
+	async fetch(
+		request: Request,
+		env: Env,
+		ctx: ExecutionContext,
+	): Promise<Response> {
+		return new Response("Hello World!");
+	},
+};
