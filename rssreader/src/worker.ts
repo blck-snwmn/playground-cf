6c1d0906332b3be86fa35e3e85f91e1b756fcff7
blck-snwmn <whitesnowmancreator@gmail.com>
2023-11-26 14:56:11 +0900

    Add rss reader sample

--- rssreader/src/worker.ts
+++ rssreader/src/worker.ts
@@ -1,4 +1,19 @@
-export type Env = never;
+import { XMLParser } from "fast-xml-parser";
+export type Env = {
+	URL: string;
+};
+
+type XMLContemt = {
+	RDF: {
+		item: XMLItem[];
+	};
+};
+
+type XMLItem = {
+	title: string;
+	date: string;
+	link: string;
+};
 
 export default {
 	async fetch(
@@ -6,6 +21,26 @@ export default {
 		env: Env,
 		ctx: ExecutionContext,
 	): Promise<Response> {
-		return new Response("Hello World!");
+		const resp = await fetch(env.URL);
+		if (!resp.ok) {
+			return new Response("Error");
+		}
+		const text = await resp.text();
+
+		const parser = new XMLParser({
+			removeNSPrefix: true,
+		});
+		const json: XMLContemt = parser.parse(text);
+
+		const items: XMLItem[] = [];
+		for (const i of json.RDF.item) {
+			items.push({
+				title: i.title,
+				date: i.date,
+				link: i.link,
+			});
+		}
+
+		return new Response(JSON.stringify(items));
 	},
 };
