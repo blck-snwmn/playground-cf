6db661003f2492691f144ae5af79b5a81577e278
blck-snwmn <whitesnowmancreator@gmail.com>
2023-12-11 02:42:28 +0900

    Add zte sample

---
+++ ztesample/README.md
@@ -0,0 +1,4 @@
+# Memo
+This sample uses jose as jwt validation.  
+As of 2023-12-11 Cloudflare worker does not support KeyObjects, which results in an error.   
+FYI: https://developers.cloudflare.com/workers/runtime-apis/nodejs/crypto/
---
+++ ztesample/src/index.ts
@@ -0,0 +1,26 @@
+import { Hono } from "hono";
+import * as jose from "jose";
+import { JsonWebTokenError } from "jsonwebtoken";
+
+type Bindings = {
+	JWK_URL: string;
+};
+
+const app = new Hono<{ Bindings: Bindings }>();
+
+app.get("/jwt", async (c) => {
+	console.log("jwt");
+	const token = c.req.raw.headers.get("cf-access-jwt-assertion");
+	if (!token) {
+		return c.json({ error: "no token" }, { status: 400 });
+	}
+	const jwks = jose.createRemoteJWKSet(new URL(c.env.JWK_URL));
+	const { payload, protectedHeader } = await jose.jwtVerify(token, jwks, {
+		algorithms: ["RS256"],
+	});
+	console.log(payload);
+	console.log(protectedHeader);
+	return c.json({ payload, protectedHeader });
+});
+
+export default app;
