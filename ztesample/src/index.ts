3b95ba3edd0b3a43fd39e0adda7d0c852b47dfe7
blck-snwmn <whitesnowmancreator@gmail.com>
2023-12-11 23:57:48 +0900

    Verify jwt

--- ztesample/src/index.ts
+++ ztesample/src/index.ts
@@ -2,7 +2,8 @@ import { Hono } from "hono";
 import * as jose from "jose";
 
 type Bindings = {
-	JWK_URL: string;
+	TEAM_NAME: string;
+	APP_AUD: string;
 };
 
 const app = new Hono<{ Bindings: Bindings }>();
@@ -13,13 +14,21 @@ app.get("/jwt", async (c) => {
 	if (!token) {
 		return c.json({ error: "no token" }, { status: 400 });
 	}
-	const jwks = jose.createRemoteJWKSet(new URL(c.env.JWK_URL));
-	const { payload, protectedHeader } = await jose.jwtVerify(token, jwks, {
-		algorithms: ["RS256"],
-	});
-	console.log(payload);
-	console.log(protectedHeader);
-	return c.json({ payload, protectedHeader });
+	const jwksURL = new URL(
+		`https://${c.env.TEAM_NAME}.cloudflareaccess.com/cdn-cgi/access/certs`,
+	);
+	const jwks = jose.createRemoteJWKSet(jwksURL);
+	try {
+		const { payload, protectedHeader } = await jose.jwtVerify(token, jwks, {
+			algorithms: ["RS256"],
+			audience: c.env.APP_AUD,
+		});
+		console.log("jwt verified");
+		return c.json({ message: "success" });
+	} catch (error) {
+		console.log(error);
+		return c.json({ error: "invalid token" }, { status: 400 });
+	}
 });
 
 export default app;
