bb5f666fd89b52fa61e6db9c4c312279414e80b6
blck-snwmn <whitesnowmancreator@gmail.com>
2024-03-02 22:17:28 +0900

    Fix lint

--- remix-vite/load-context.ts
+++ remix-vite/load-context.ts
@@ -6,12 +6,12 @@ import { type PlatformProxy } from "wrangler";
 // Need this empty interface so that typechecking passes
 // even if no `wrangler.toml` exists.
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
-interface Env {}
+type Env = {};
 
 type Cloudflare = Omit<PlatformProxy<Env>, "dispose">;
 
 declare module "@remix-run/cloudflare" {
-  interface AppLoadContext {
-    cloudflare: Cloudflare;
-  }
+	interface AppLoadContext {
+		cloudflare: Cloudflare;
+	}
 }
--- remix-vite/vite.config.ts
+++ remix-vite/vite.config.ts
@@ -1,10 +1,10 @@
 import {
-  vitePlugin as remix,
-  cloudflareDevProxyVitePlugin as remixCloudflareDevProxy,
+	cloudflareDevProxyVitePlugin as remixCloudflareDevProxy,
+	vitePlugin as remix,
 } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [remixCloudflareDevProxy(), remix(), tsconfigPaths()],
+	plugins: [remixCloudflareDevProxy(), remix(), tsconfigPaths()],
 });
