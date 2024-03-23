57a162bd283342a030b5218bdc96c2b6b1588f9e
blck-snwmn <whitesnowmancreator@gmail.com>
2024-03-24 01:02:06 +0900

    Add new project

---
+++ workerrequesttest/src/index.ts
@@ -0,0 +1,11 @@
+export type Env = {};
+
+export default {
+	async fetch(
+		request: Request,
+		env: Env,
+		ctx: ExecutionContext,
+	): Promise<Response> {
+		return new Response("Hello World!");
+	},
+};
