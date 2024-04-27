47691287d5fa4e1d20be8f837bc96562fd4fe6b2
blck-snwmn <whitesnowmancreator@gmail.com>
2024-04-27 23:41:49 +0900

    Generate img by stable-diffusion-xl-lightning

--- ai-imggen/src/index.ts
+++ ai-imggen/src/index.ts
@@ -1,18 +1,23 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
 export default {
-	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World!');
+	async fetch(
+		request: Request,
+		env: Env,
+		ctx: ExecutionContext,
+	): Promise<Response> {
+		const inputs = {
+			prompt:
+				"Icon of a British Shorthair cat in profile warming up in a room.",
+		};
+
+		const response = await env.AI.run(
+			"@cf/bytedance/stable-diffusion-xl-lightning",
+			inputs,
+		);
+
+		return new Response(response, {
+			headers: {
+				"content-type": "image/png",
+			},
+		});
 	},
 };
