18380f73e0de29c95370e7d41d15d009a7d8533e
blck-snwmn <whitesnowmancreator@gmail.com>
2024-04-29 02:42:33 +0900

    Add sse sample

---
+++ ai-sse-worker/src/index.ts
@@ -0,0 +1,35 @@
+import { Ai } from "@cloudflare/ai";
+import { events } from "fetch-event-stream";
+import { Hono } from "hono";
+import { streamText } from "hono/streaming";
+
+type Bindings = {
+	AI: Ai;
+};
+
+const app = new Hono<{ Bindings: Bindings }>();
+
+app.get("/", async (c) => {
+	const messages = [
+		{ role: "system", content: "You are a friendly assistant" },
+		{
+			role: "user",
+			content: "What is the origin of the phrase Hello, World",
+		},
+	];
+
+	const apiStream = (await c.env.AI.run("@cf/meta/llama-3-8b-instruct", {
+		messages,
+		stream: true,
+	})) as ReadableStream;
+
+	return streamText(c, async (stream) => {
+		const chunks = events(new Response(apiStream));
+		for await (const chunk of chunks) {
+			const data = JSON.parse(chunk.data!);
+			stream.write(data.response);
+		}
+	});
+});
+
+export default app;
